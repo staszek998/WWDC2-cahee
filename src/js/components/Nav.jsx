e7dce339e990133a039aeb6e969cf3b6a6af6e9b
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-05-25 11:22:29 +0200

    Remove Navbar background

--- src/js/components/Nav.jsx
+++ src/js/components/Nav.jsx
@@ -4,44 +4,42 @@ class Nav extends Component {
   render() {
     return (
       <nav className="navbar">
-        <div className="container">
-          <a href="#" className="navbar-brand logo">
-            Cahee
-          </a>
+        <a href="#" className="navbar-brand logo">
+          Cahee
+        </a>
 
-          <ul className="nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                HOME
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                ABOUT
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                SERVICES
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                GALLERY
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                BLOG
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                CONTACT
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link active" href="#">
+              HOME
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              ABOUT
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              SERVICES
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              GALLERY
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              BLOG
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              CONTACT
+            </a>
+          </li>
+        </ul>
       </nav>
     );
   }
