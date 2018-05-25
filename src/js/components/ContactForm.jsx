6ca5ba9ec82c1ccca4c5ed80416a639445aac795
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-05-25 11:27:24 +0200

    Correct Contact Form styling

--- src/js/components/ContactForm.jsx
+++ src/js/components/ContactForm.jsx
@@ -6,27 +6,27 @@ class ContactForm extends Component {
       <form action="submit" className="form-group">
         <input
           type="text"
-          className="form-control mb-1 rounded-0 border-0"
+          className="form-control mb-1 rounded-0 border-0 text-white"
           placeholder="Name"
         />
         <input
           type="tel"
-          className="form-control mb-1 rounded-0 border-0"
+          className="form-control mb-1 rounded-0 border-0 text-white"
           placeholder="Phone number"
         />
         <input
           type="email"
-          className="form-control mb-1 rounded-0 border-0"
+          className="form-control mb-1 rounded-0 border-0 text-white"
           placeholder="E-mail address"
         />
         <textarea
-          className="form-control mb-1 rounded-0 border-0"
+          className="form-control mb-1 rounded-0 border-0 text-white"
           rows="10"
           placeholder="Message"
         />
         <input
           type="submit"
-          className="btn btn-default rounded-0 mb-5 border-0 shadow"
+          className="btn btn-default rounded-0 mb-5 border-0 shadow text-white"
           value="SEND"
         />
       </form>
